cf45476f8160164fbbd2fed3b67c0ed5288bdd6d
paulalextech <psteane123@hotmail.com>
2024-03-19 23:18:01 +0000

    imported all elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
 export default function App() {
 	return (
@@ -12,6 +14,8 @@ export default function App() {
 				<Route path="/about" element={<About />} />
 				<Route path="/sign-in" element={<SignIn />} />
 				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/projects" element={<Projects />} />
 			</Routes>
 		</BrowserRouter>
 	);
