b51e9d9242f5caa93ac41e4e587e102cdec19470
paulalextech <psteane123@hotmail.com>
2024-03-19 23:59:45 +0000

    color to logo. add form search box

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,27 @@
-import { Navbar } from 'flowbite-react';
+import { Navbar, TextInput } from 'flowbite-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export default function Header() {
 	return (
 		<Navbar className="border-b-2">
-			<span>zeros & ones</span>
+			<Link
+				to="/"
+				className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+			>
+				<span className="px-2 py-1 rounded-lg bg-gradient-to-r from-pink-500 to-orange-500 text-white">
+					Zeros & Ones{' '}
+				</span>
+				Blog
+			</Link>
+			<form>
+				<TextInput
+					typeof="text"
+					placeholder="search.."
+					rightIcon={AiOutlineSearch}
+				/>
+			</form>
 		</Navbar>
 	);
 }
