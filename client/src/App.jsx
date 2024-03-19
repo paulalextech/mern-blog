001158a5065e88d524aae37109146a48161bc5e9
paulalextech <psteane123@hotmail.com>
2024-03-19 23:13:40 +0000

    set up routes to pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default function App() {
-	return <h1 className="text-3xl text-purple-500">app</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
