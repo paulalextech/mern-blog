4baad4877a25d91912ea905aab8e2b7e890a5518
paulalextech <psteane123@hotmail.com>
2024-03-28 15:07:01 +0000

    asigned variable to auth controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,14 @@
 export const signup = async (req, res) => {
-	console.log(req.body);
+	const { username, email, password } = req.body;
+
+	if (
+		!username ||
+		!email ||
+		!password ||
+		username === '' ||
+		email === '' ||
+		password === ''
+	) {
+		return res.status(400).json({ message: 'All fields are required' });
+	}
 };
